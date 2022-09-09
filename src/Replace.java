d90b583a55c66c65091833caa7186d6ac5ff88c9
Your Name <you@example.com>
2022-09-09 13:39:46 +0530

    added: logic for replace number

---
+++ src/Replace.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Replace {
+    public static void main(String[] args)
+    {
+        int number;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        number = sc.nextInt();
+        String str = Integer.toString(number);
+        int len = str.length();
+        String str1 = "";
+        //use the logic to replace all 0's with 1 in a given integer
+        for(int i = 0 ; i < len ; i++)
+        {
+            if(str.charAt(i) == '0')
+                str1 = str1 + '1';
+            else
+                str1 = str1 + str.charAt(i);
+        }
+        System.out.println("Converted number is: "+str1);
+        //closing scanner class(not compulsory, but good practice)
+        sc.close();
+    }
+}
